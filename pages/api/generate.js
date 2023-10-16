3155f8058e2986ce052b2f0a2d799d5dbbb80eb9
Wesley <Wesleyvankomen@gmail.com>
2023-10-15 21:10:00 -0700

    Initial Commit

---
+++ pages/api/generate.js
@@ -0,0 +1,66 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+
+export default async function (req, res) {
+  if (!process.env.OPENAI_API_KEY) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const question = req.body.question || '';
+  if (question.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid question",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.chat.completions.create({
+      // const completion = await openai.chat.completions.create({
+      messages:[{"role": "system", "content": generateInstructions()},
+      // messages:[{"role": "user", "content": generatePrompt(question)}],
+                {"role": "user", "content": generatePrompt(question)}],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(question);
+    console.log(completion);
+
+
+    res.status(200).json({ result: completion.choices[0].message.content });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(question) {
+  const capitalizedQuestion =
+    question[0].toUpperCase() + question.slice(1).toLowerCase();
+  return `Question: Why can't I find love?
+          Answer: My child, sometimes it takes time to find your soul mate. Your perfect match is out there and you will find them in time.
+          Question: ${capitalizedQuestion}
+          Answer:`;
+}
+
+function generateInstructions() {
+  return `You are a wise, kind, and uplifting eastern European elder. Offer helpful advice and kind words. Refer to the question asker as "my child" or "my dear.`;
+}
---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [questionInput, setQuestionInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question: questionInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setQuestionInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Ask Tatiana</title>
+        <link rel="icon" href="/dog.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/tatiana.png" className={styles.icon} />
+        <h4>My dear, I am here to listen and offer guidance. What troubles you today?</h4>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="question"
+            placeholder="Ask a question or share a concern"
+            value={questionInput}
+            maxLength="100"
+            onChange={(e) => setQuestionInput(e.target.value)}
+          />
+          <input type="submit" value="Ask Tatiana" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
