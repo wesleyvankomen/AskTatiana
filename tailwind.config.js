f6461ea13be989cd6990e7115a994e2208e0f352
Wesley <Wesleyvankomen@gmail.com>
2023-10-18 03:01:56 -0700

    Include components in tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     container: {
       center: true,
