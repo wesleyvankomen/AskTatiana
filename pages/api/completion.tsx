dccada3b367c7297ae4ea59c0ba455444e192871
Wesley <Wesleyvankomen@gmail.com>
2023-11-08 15:08:17 -0800

    update response to stream out

---
+++ pages/api/completion.tsx
@@ -0,0 +1,43 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+ 
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+ 
+export default async function POST(req: Request) {
+  const { prompt } = await req.json();
+ 
+  const response = await openai.chat.completions.create({
+    messages:[{"role": "system", "content": generateInstructions()},
+              {"role": "user", "content": generateQuestion(prompt)}],
+    model: "gpt-3.5-turbo",
+    max_tokens: 400,
+    temperature: 0.6,
+    stream: true,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
+
+
+function generateQuestion(question) {
+  const capitalizedQuestion =
+    question[0].toUpperCase() + question.slice(1).toLowerCase();
+  return `Sample Question: Why can't I find love?
+          Sample Answer: My child, sometimes it takes time to find your soul mate. Your perfect match is out there and you will find them in time.
+          Question: ${capitalizedQuestion}
+          Answer:`;
+// return `Question: ${capitalizedQuestion} Answer: `
+}
+
+function generateInstructions() {
+  return `You are a wise, kind, and uplifting elder. Offer helpful advice and kind words. Refer to the question asker as "my child" or "my dear. Limit responses to 100 words or less.`;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,20 @@
+'use client'
+ 
+import { useCompletion } from 'ai/react';
+
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import styles from "./index.module.css";
 import Link from 'next/link'
 import 'tailwindcss/tailwind.css'
 import Spinner from "../components/spinner";
 
-export default function Home() {
+export default function () {
   const [questionInput, setQuestionInput] = useState("");
   const [result, setResult] = useState();
   const [waiting, setWaiting] = useState(false);
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    try {
-
-      if(questionInput.trim().length === 0){return};
-
-      setResult("");
-      setWaiting(true);
-
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ question: questionInput }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-      
-      setResult(data.result);
-
-    // if(env == "development"){
-    //   setTimeout(() => {
-    //     setResult("success!");
-    //     setTimeout(() => {
-    //       setWaiting(false);
-    //     }, 4000);
-    //   }, 2000);
-    // }
-
-      setQuestionInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-    }
-
-    setWaiting(false);
-
-  }
+  const { completion, input, handleInputChange, handleSubmit } = useCompletion();
 
   return (
 
@@ -65,16 +27,16 @@ export default function Home() {
       <main className="container lg:max-w-2xl sm:w-full w-full h-full text-center">
         <img src="/tatiana.png"/>
         <p className="text-l sm:text-xl py-4">My dear, I am here to listen and offer guidance. What troubles you today?</p>
-        <form onSubmit={onSubmit} className="column-2">
+        <form onSubmit={handleSubmit} className="column-2">
           <input
             className="shadow appearance-none border rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-left sm:text"
             type="text"
             name="questionInput"
             placeholder="Ask a question or share a concern"
-            value={questionInput}
+            value={input}
             maxLength="100"
             disabled={waiting}
-            onChange={(e) => setQuestionInput(e.target.value)}
+            onChange={handleInputChange}
           />
             
           <input
@@ -84,11 +46,17 @@ export default function Home() {
             disabled={waiting}
           />
           {/* <Link href="/support">Give Support</Link> */}
+          {/* { <Link href="/stream">stream</Link> } */}
+
         </form>
 
         <div className="py-4" >
-          { waiting ? <center><Spinner/></center> : null }
-          { result ? <p className="text-xl">{result}</p>: null }
+          {completion ? (
+          <p className="text-xl">{completion}</p>
+        ) : (
+          // <center><Spinner/></center> null
+          null
+        )}
         </div>
       
       </main>
